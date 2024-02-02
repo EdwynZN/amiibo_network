a914bac546d2690b1365d621ef754b9ea019618e
Edwin Zambrano <edwinzn9@gmail.com>
2024-02-01 21:33:38 -0600

    - fix import of new json format

--- lib/model/amiibo.dart
+++ lib/model/amiibo.dart
@@ -6,8 +6,8 @@ part 'amiibo.g.dart';
 int boolToInt(bool? value) => value ?? false ? 1 : 0;
 bool intToBool(int? value) => value == 1;
 
-List<Amiibo> entityFromMap(Map<String, dynamic>? amiibo) =>
-    List<Amiibo>.from(amiibo!["amiibo"].map((x) => Amiibo.fromJson(x)));
+List<Amiibo> entityFromMap(Map<String, dynamic> amiibo) =>
+    List<Amiibo>.from(amiibo["amiibo"].map((x) => Amiibo.fromJson(x)));
 
 @freezed
 class Amiibo with _$Amiibo {
