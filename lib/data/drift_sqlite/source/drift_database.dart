a5c801e52130e5e9fe9a1c1e1934c9b8cb42302f
EdwynZN <edwinzn9@gmail.com>
2024-04-28 23:53:55 -0600

    migration table

--- lib/data/drift_sqlite/source/drift_database.dart
+++ lib/data/drift_sqlite/source/drift_database.dart
@@ -1,11 +1,13 @@
 import 'dart:io';
 
 import 'package:amiibo_network/data/drift_sqlite/source/amiibo_dao.dart';
+import 'package:amiibo_network/utils/preferences_constants.dart';
 import 'package:drift/drift.dart';
 import 'package:drift/native.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:sqlite3/sqlite3.dart';
 import 'package:sqlite3_flutter_libs/sqlite3_flutter_libs.dart';
@@ -13,30 +15,6 @@ import 'package:sqlite3_flutter_libs/sqlite3_flutter_libs.dart';
 part 'drift_database.g.dart';
 
 final databaseProvider = Provider((ref) => AppDatabase());
-
-/* class AmiiboTable extends Table {
-  IntColumn get key => integer().autoIncrement()();
-  TextColumn get tag => text().named('id').nullable()();
-  TextColumn get amiiboSeries => text()();
-  TextColumn get character => text()();
-  TextColumn get gameSeries => text()();
-  TextColumn get name => text()();
-  TextColumn get au => text()();
-  TextColumn get eu => text()();
-  TextColumn get jp => text()();
-  TextColumn get na => text()();
-  TextColumn get type => text().nullable()();
-  IntColumn get cardNumber => integer().nullable()();
-}
-
-class AmiiboUserPreferencesTable extends Table {
-  IntColumn get key => integer().autoIncrement()();
-  IntColumn get amiiboTableKey =>
-      integer().named('amiibo_key').references(AmiiboTable, #key).unique()();
-  IntColumn get boxed => integer().withDefault(const Constant(0))();
-  IntColumn get opened => integer().withDefault(const Constant(0))();
-  BoolColumn get wishlist => boolean().withDefault(const Constant(false))();
-} */
 
 @DriftDatabase(
   //tables: [AmiiboTable, AmiiboUserPreferencesTable],
@@ -58,19 +36,107 @@ class AppDatabase extends _$AppDatabase {
         await m.createAll();
       },
       beforeOpen: (details) async {
-        print(details.wasCreated);
-        print('now: ${details.versionNow} - before: ${details.versionBefore}');
+        await customStatement('PRAGMA foreign_keys = ON');
       },
       onUpgrade: (Migrator m, int from, int to) async {
+        await customStatement('PRAGMA foreign_keys = OFF');
         if (from < 2) {
-          // we added the dueDate property in the change from version 1 to
-          // version 2
-          //await m.addColumn(todos, todos.dueDate);
+          await transaction(() async {
+            await customStatement('ALTER TABLE amiibo RENAME TO _amiibo_old;');
+            await customStatement('''
+              CREATE TABLE IF NOT EXISTS amiibo(
+                key INTEGER PRIMARY KEY AUTOINCREMENT,
+                id TEXT,
+                amiiboSeries TEXT NOT NULL,
+                character TEXT NOT NULL,
+                gameSeries TEXT NOT NULL,
+                name TEXT NOT NULL,
+                au TEXT,
+                eu TEXT,
+                jp TEXT,
+                na TEXT,
+                type TEXT NOT NULL,
+                wishlist INTEGER,
+                owned INTEGER;
+              )
+            ''');
+            await customStatement('''INSERT INTO
+                amiibo(id, amiiboSeries, character, gameSeries, character,
+                  name, au, eu, jp, na, type, wishlist, owned)
+                SELECT id, amiiboSeries, character, gameSeries, character, 
+                  name, au, eu, jp, na, type, wishlist, owned
+                FROM _amiibo_old ORDER BY id;
+              ''');
+            await customStatement('DROP TABLE _amiibo_old;');
+          });
         }
         if (from < 3) {
           // we added the priority property in the change from version 1 or 2
           // to version 3
           //await m.addColumn(todos, todos.priority);
+          final SharedPreferences prefs = await SharedPreferences.getInstance();
+          List<Map<String, dynamic>> table;
+          await transaction(() async {
+            table = await customSelect('SELECT * FROM date;')
+                .map((p0) => p0.data)
+                .get();
+            table.forEach((date) {
+              if (date['id'] == '1')
+                prefs.setString(sharedDateDB, date['lastUpdated']);
+              if (date['id'] == '2')
+                prefs.setString(sharedOldTheme, date['lastUpdated']);
+            });
+            await customStatement('DROP TABLE date;');
+          });
+        }
+        if (from < 4) {
+          await transaction(() async {
+            await customStatement('ALTER TABLE amiibo RENAME TO _amiibo_old;');
+            await customStatement('''
+              CREATE TABLE IF NOT EXISTS amiibo(
+                key INTEGER PRIMARY KEY AUTOINCREMENT,
+                id TEXT,
+                amiiboSeries TEXT NOT NULL,
+                character TEXT NOT NULL,
+                gameSeries TEXT NOT NULL,
+                name TEXT NOT NULL,
+                au TEXT,
+                eu TEXT,
+                jp TEXT,
+                na TEXT,
+                type TEXT NOT NULL,
+                cardNumber INTEGER,
+                wishlist INTEGER,
+                owned INTEGER
+              );
+            ''');
+            await customStatement('''INSERT INTO
+                amiibo(key, id, amiiboSeries, character, gameSeries, character,
+                  name, au, eu, jp, na, type, wishlist, owned)
+                SELECT key, id, amiiboSeries, character, gameSeries, character, 
+                  name, au, eu, jp, na, type, wishlist, owned
+                FROM _amiibo_old ORDER BY id;
+              ''');
+            await customStatement('DROP TABLE _amiibo_old;');
+          });
+        }
+        if (from < 5) {
+          await transaction(() async {
+            await customStatement('ALTER TABLE amiibo RENAME TO _amiibo_old;');
+            await m.createAll();
+            await customInsert('''INSERT INTO
+                amiibo(key, id, amiiboSeries, character, gameSeries, character,
+                  name, au, eu, jp, na, type)
+                SELECT key, id, amiiboSeries, character, gameSeries, character, 
+                  name, au, eu, jp, na, type
+                FROM _amiibo_old ORDER BY id;
+              ''');
+            await customInsert('''INSERT OR REPLACE INTO 
+                amiibo_user_preferences(amiibo_key, opened, boxed, wishlist)
+                SELECT key, owned, 0, wishlist FROM _amiibo_old ORDER BY id;
+              ''');
+            await customStatement('DROP TABLE _amiibo_old;');
+          });
         }
       },
     );
